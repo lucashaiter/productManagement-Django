baac48dc53b257db687f5c210cb1883b48b8e0d9
lucashaiter <lucas.leoni.haiter@gmail.com>
2025-10-07 12:06:15 -0300

    View Lista Produto Produto

--- loja/urls.py
+++ loja/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
-from . import views
+from .views import views, ProdutoListView, ProdutoDetailView
 
 appName = "loja"
 
 urlpatterns = [
-    # path('', views.paginaInicialLoja, name="loja"),
-    # path('produto/<slug:produtoSlug>/', views.verProdutoPorSlug, name="ver_produtos"), 
-    path('lista-produtos/', views.listaProdutosView, name="lista_produtos"),
-    path('lista-produtos/produto/<int:produto_id>/', views.detalheProdutoView, name="detalhe_produto"),
     path('sobre/', views.sobreLojaView, name="sobre"),
     path('contato/', views.contatoLojaView, name="contato"),
+    
+    path('lista-produtos/', views.listaProdutosView, name="lista_produtos"),
+    path('lista-produtos/produto/<int:produto_id>/', views.detalheProdutoView, name="detalhe_produto"),
+
     path('lista-produtos-valiosos/', views.listaProdutosValiososView, name="lista_produtos_valiosos"),
-    path('lista-produtos-valiosos/produtoValioso/<int:produto_id>/', views.detalheProdutoValiosoView, name="detalhe_produto_valioso")
+    path('lista-produtos-valiosos/produtoValioso/<int:produto_id>/', views.detalheProdutoValiosoView, name="detalhe_produto_valioso"),
+    
+    path('lista-produtos-produtos/', ProdutoListView.as_view() ,name="lista_produtos_produtos"),
+    path('lista-produtos-produto/produtoProduto/<int:pk>/', ProdutoDetailView.as_view(), name="detalhe_produto_produto"),
 ]
--- loja/views.py
+++ loja/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from loja.models import Produto
+from .models import Produto
+from django.views.generic import ListView, DetailView
 
+# Defs
 def paginaInicialLoja(request):
     return render(request, 'base_loja.html')
 
-# def verProdutoPorSlug(request, produtoSlug):
-#     return HttpResponse(f"<h1> Exibindo detalhes do produto: {produtoSlug}</h1>")
+# -------------------------------------------------------------------------
 
 def listaProdutosView(request):
     produtosDisponiveis = Produto.objects.filter(estoque__gt = 0)
@@ -17,12 +18,18 @@ def detalheProdutoView(request, produto_id):
     produto = get_object_or_404(Produto, id = produto_id)
     contexto = {'produto': produto}
     return render(request, 'loja/detalheProduto.html', contexto)
+
+# -------------------------------------------------------------------------
     
 def sobreLojaView(request):
     return render(request, 'loja/sobre.html')
 
+# -------------------------------------------------------------------------
+
 def contatoLojaView(request):
     return render(request, 'loja/contato.html')
+
+# -------------------------------------------------------------------------
 
 def listaProdutosValiososView(request):
     produtosValiosos = Produto.objects.filter(estoque__gt = 5, preco__gt = 500).order_by("preco")
@@ -33,3 +40,31 @@ def detalheProdutoValiosoView(request, produto_id):
     produtoValioso = get_object_or_404(Produto, id = produto_id)
     contexto = {'produtoValioso': produtoValioso}
     return render(request, 'loja/detalheProdutoValioso.html', contexto)
+
+# -------------------------------------------------------------------------
+
+# def produtoListFBV(request):
+#     produtosEstoque = Produto.objects.filter(estoque__gt = 0).order_by('nome')
+#     contexto = {
+#         'produtos': produtosEstoque,
+#         'tituloPagina': 'Nossos produtos disponíveis'
+#     }
+    
+#     return render(request, 'loja/listaProdutosProdutos.html', contexto)
+
+# --------------------------------------------------------------------------
+
+# Classes
+class ProdutoListView(ListView):
+    model = Produto
+    template_name = 'loja/listaProdutosProdutos.html'
+    context_object_name = 'produtos'
+    
+    def get_queryset(self):
+        return Produto.objects.filter(estoque__gt = 0).order_by('nome')
+
+class ProdutoDetailView(DetailView):
+    model = Produto
+    template_name = 'loja/detalheProdutoProduto.html'
+    context_object_name = 'produto'
+    
